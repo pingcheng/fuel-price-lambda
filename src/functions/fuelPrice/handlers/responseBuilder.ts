30581a1c2d568e609faaf50ca04ff016001dafaa
Ping Cheng <neroc7@gmail.com>
2022-12-30 17:27:35 +1100

    add fuel type

--- src/functions/fuelPrice/handlers/responseBuilder.ts
+++ src/functions/fuelPrice/handlers/responseBuilder.ts
@@ -2,6 +2,7 @@ export const buildResponse = (price: Price, address: string): string => {
   const lines = [];
 
   lines.push(`:sunglasses: Found the lowest address!`);
+  lines.push(`:fuelpump: ${price.type}`);
   lines.push(`:moneybag: $${price.price.toFixed(2)}`);
   lines.push(`:round_pushpin: ${address}`);
   lines.push(`:world_map: ${price.location.lat}, ${price.location.lng}`);
